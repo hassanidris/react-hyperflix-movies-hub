22496662e1223655540fdd30b9f5c5194f7751fb
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-29 20:40:09 +0100

    fixing pulling name

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -9,7 +9,7 @@ const Movies = ({title, items}) => {
       <div>
         {items.results.length > 0 && items.results.map((item, key)=>(
           <div key={key}>
-            <p>{item.title}</p>
+            <p>{item.title || item.name}</p>
             <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} alt={item.original_title}/>
           </div>
         ))}
