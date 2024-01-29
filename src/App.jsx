aebdfed46610e76273920236c2804151cc9f9ee4
IngridAkeida <ingridsanchesp@gmail.com>
2024-01-29 19:14:11 +0100

    add - fetch the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+//components
+import Movies from "./components/Movies";
+//data 
+import apiConfig from "./data/apiConfig";
+//hooks 
+import { useEffect, useState } from "react";
 
+export default function App() {
 
-function App() {
-  
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      
+      //catching the lists 
+      let list = await apiConfig.getHomeList();
+      setMovieList(list)
+      console.log(list)
+    }
+
+    loadAll();
+  }, [])
 
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <header>Header</header>
+      <div>
+        <div>
+          {movieList.map((item, key)=>(
+            <Movies key={key} title={item.title} items={item.items}/>
+          ))}
+        </div>
+      </div>
+      <div>footer</div>
+    </div>
+  );
 }
-
-export default App
---
+++ src/components/Movies.jsx
@@ -0,0 +1,21 @@
+const Movies = ({title, items}) => {
+
+  
+  console.log(items)
+
+  return(
+    <div>
+      <h2>{title}</h2>
+      <div>
+        {items.results.length > 0 && items.results.map((item, key)=>(
+          <div key={key}>
+            <p>{item.title}</p>
+            <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} alt={item.original_title}/>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Movies;
