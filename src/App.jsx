1526a363ff2084ec71e440c7d3c392fc52f8bf56
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-30 20:00:45 +0100

    Adjusting Router and adding footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
+import Navbar from "./components/NavBar";
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/:id" element={ <Details/> } />
         
       </Routes>
+      <Footer />
+      </BrowserRouter>
     </div>
   )
 }
