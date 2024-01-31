d1979b474a206ab5f548d8b2f26b8cdae4b21c00
IngridAkeida <ingridsanchesp@gmail.com>
2024-01-31 13:17:03 +0100

    update commit

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -6,7 +6,7 @@ const HeroBanner = ({items}) => {
   console.log(items)
 
   return(
-    <div className="bg-slate-900 text-white">
+    <div>
       <div>
         <h2>{items.title}</h2>
         <img src={`https://image.tmdb.org/t/p/w200${items.poster_path}`} alt={items.title}/>
