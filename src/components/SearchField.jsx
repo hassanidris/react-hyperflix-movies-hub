b3e68b38ad449d651fc909634175980c795e995c
Hassen Ahmed <hassan.creative@gmail.com>
2024-02-02 08:28:33 +0100

    fixing search field

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,12 +1,15 @@
+import { FaSearch } from "react-icons/fa";
+
 const SearchField = () => {
   
 
   return(
-    <div> 
+    <form className=" flex justify-center items-center gap-2"> 
+      <input className=" rounded-full border-2 w-auto md:w-[400px] h-10 border-m_gold" type="text" />
 
-      SearchField
+      <FaSearch className=" text-m_gold" size={30} />
       
-    </div>
+    </form>
   )
 }
 
