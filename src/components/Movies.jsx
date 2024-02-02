77e8cd8d91185a0a6b92b0eddb3a225f10e05ff1
Hassen Ahmed <hassan.creative@gmail.com>
2024-02-02 23:32:59 +0100

    Shifting all changes

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -37,11 +37,13 @@ const Movies = ({title, items, key}) => {
             <div className=" w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 shadow-lg" key={key}>
               <img className=" w-full h-auto block" src={`https://image.tmdb.org/t/p/w500${item.backdrop_path || item.poster_path}`} alt={item.original_title}/>
               <div className=" absolute top-0 left-0 w-full h-full hover:bg-m_black/80 opacity-0 hover:opacity-100 text-m_white">
-              <p className=" text-m_white whitespace-normal font-open_sans text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{item.title || item.name}</p>
+              <div className='flex flex-col justify-center items-center gap-2 h-full text-center'>
+                <p className=" text-m_white whitespace-normal font-open_sans text-xs md:text-sm font-bold ">{item.title || item.name}</p>
+                <BtnDetails onClick={() => navigateToMovie(item.id)} />
+              </div>
               <p>
                 {like ? <FaHeart className=' absolute top-4 left-4 text-m_white' /> : <FaRegHeart className=' absolute top-4 left-4 text-m_white' onClick={()=>setLike(!like)}/>}
               </p>
-              <BtnDetails onClick={() => navigateToMovie(item.id)} />
               </div>
             </div>
           ))}
