e04d91d8d08bdb8b98251a86002d70034c558cb5
IngridAkeida <ingridsanchesp@gmail.com>
2024-02-03 18:30:05 +0100

    add- modal on site

--- src/components/BtnTrailer.jsx
+++
@@ -1,7 +0,0 @@
-const BtnTrailer = ({onClick}) => {
-  return(
-    <button style={{backgroundColor: 'white', color:'black'}} onClick={onClick}>Trailer</button>
-  )
-}
-
-export default BtnTrailer;
--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -1,10 +1,33 @@
-const Trailer = ({videoKey}) => {
+import { useState } from "react";
+import Modal from 'react-modal';
 
-  return videoKey? (
-    <div style={{color: 'red', marginTop: 200 }}>
-      <div>
-        <iframe src={`https://www.youtube.com/embed/${videoKey}`}></iframe>
-      </div>
+
+const Trailer = ({videoKey}) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return videoKey ? (
+    <div>
+      <button onClick={openModal}>See trailer</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Trailer"
+      >
+        <div style={{color: 'red', marginTop: 200 }}>
+          <div>
+            <iframe src={`https://www.youtube.com/embed/${videoKey}`}></iframe>
+          </div>
+        </div>
+       
+      </Modal>
     </div>
   ): null
 }
