60ee2f795a6fdac0141cfb191be0c60737048178
IngridAkeida <ingridsanchesp@gmail.com>
2024-02-06 10:23:56 +0100

    adding and being visible - style on search component

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -10,7 +10,6 @@ const classNameInput = "block w-[50vw] p-3 ps-10 text-sm text-gray-900 border bo
 const SearchField = () => {
   const navigate = useNavigate();
 
-  //logic
   const [query, setQuery] = useState('');
 
   const searchMovies = (e) => {
@@ -22,26 +21,28 @@ const SearchField = () => {
 
   return(
     <form onSubmit={searchMovies}>   
-      <label className={classNameLabel}>Search</label>
-      <div className="relative">
+    <label className={classNameLabel}>Search</label>
+    <div className="relative">
         <div className={classNameWrapped}>
-            <svg className="w-4 h-4 text-m_gold dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" /*stroke-linecap="round" stroke-linejoin="round" stroke-width="2"*/ d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
         <input 
           type="text" 
           id="default-search" 
-          className={classNameInput} 
           placeholder="Search for Movies..." 
           value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button type="submit" className="text-m_gold">Here</button>
-      </div>
-    </form>
+          onChange={(e) => setQuery(e.target.value)} 
+          className={classNameInput}/>
+        <button type="submit" className="text-white absolute end-1.5 bottom-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 bg-m_darkGrey text-m_gold ml-2 bg-blue-500 rounded btn-outline flex justify-center items-center gap-1">Search</button>
+    </div>
+</form>
   )
 }
 
 export default SearchField;
+
+
+
 
