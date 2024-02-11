b53de86a631ab51aa4bea747d694d4f363f43bb7
Hassen Ahmed <hassan.creative@gmail.com>
2024-02-11 05:13:44 +0100

    building backend

--- src/components/header/SearchField.jsx
+++ src/components/header/SearchField.jsx
@@ -2,41 +2,62 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-
 const SearchField = () => {
   const navigate = useNavigate();
 
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   const searchMovies = (e) => {
     e.preventDefault();
-    if (query.trim() !== '') {
+    if (query.trim() !== "") {
       navigate(`/search-results/${query}`);
-      setQuery('');
-    } 
-  }
+      setQuery("");
+    }
+  };
 
-  return(
- 
-
-    <form className=" w-[70vw] sm:w-[50vw]" onSubmit={searchMovies}>   
-    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div className="relative">
+  return (
+    <form className=" w-[70vw] sm:w-[50vw]" onSubmit={searchMovies}>
+      <label
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      >
+        Search
+      </label>
+      <div className="relative">
         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg className="w-4 h-4 text-m_gold dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
+          <svg
+            className="w-4 h-4 text-m_gold dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
         </div>
-        <input type="search" id="default-search" className="block w-full p-4 ps-8 text-sm text-gray-900 border border-m_lightGrey rounded-lg bg-m_lightGrey focus:ring-m_gold focus:bg-m_white focus:border-m_gold placeholder-m_gold" placeholder="Search for a movie..." value={query}
-          onChange={(e) => setQuery(e.target.value)} />
-        <button type="submit" className=" text-m_white absolute end-2.5 bottom-2.5 bg-m_gold hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-m_gold font-medium rounded-lg text-sm px-4 py-2 dark:bg-m_gold dark:hover:bg-m_gold dark:focus:ring-m_gold">Search</button>
-    </div>
-</form>
-  )
-}
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full p-4 ps-8 text-sm text-gray-900 border border-m_lightGrey rounded-lg bg-m_lightGrey focus:ring-m_gold focus:bg-m_white focus:border-m_gold placeholder-m_gold"
+          placeholder="Search for a movie..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          type="submit"
+          className=" text-m_white absolute end-2.5 bottom-2.5 bg-m_gold hover:bg-m_lightGrey hover:text-m_gold border border-m_gold font-medium rounded-lg text-sm px-4 py-2"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default SearchField;
-
-
-
--- src/components/ui-components/BtnDetails.jsx
+++ src/components/ui-components/BtnDetails.jsx
@@ -1,7 +1,7 @@
 import { FaInfoCircle } from "react-icons/fa";
 const BtnDetails = ({onClick, title}) => {
   return(
-    <button className="btn flex justify-center items-center gap-1" onClick={onClick}><FaInfoCircle /> {title}</button>
+    <button className="btn flex justify-center items-center gap-1 rounded-lg" onClick={onClick}><FaInfoCircle /> {title}</button>
   )
 }
 export default BtnDetails;
